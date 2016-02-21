88c977a50b5fba758ce6ae64b9a795cedbde2802
Jared Hostyn <jared-h@hotmail.com>
2016-02-21 17:35:17 +0000

    Added graphs to patient page.

---
+++ public/js/morris-mark-bar21.js
@@ -0,0 +1,16 @@
+Morris.Bar({
+	  element: 'bar-mark-feb21',
+	  data: [
+	    { y: '08:30', a: 2},
+	    { y: '10:00', a: 1},
+	    { y: '12:30', a: 3},
+	    { y: '14:00', a: 2},
+	    { y: '16:30', a: 1},
+	    { y: '18:00', a: 3},
+	    { y: '19:10', a: 2},
+	    { y: '20:30', a: 3}
+	  ],
+	  xkey: 'y',
+	  ykeys: 'a',
+	  labels: ['Number of Tokens Earned']
+});
